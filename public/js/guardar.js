cce792fc6b2d0b0e35f0bb7a35539a90033d7d89
German Martinez <germaaan@gmail.com>
2016-04-22 14:31:55 +0200

    Añade carpeta pública

---
+++ public/js/guardar.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+	var peticion;
+
+	$("#envioDatos").submit(function(event) {
+
+		if (peticion) {
+			peticion.abort();
+		}
+
+		var $formulario = $(this);
+
+		var $campos = $formulario.find("input, textarea");
+
+		var datos = $formulario.serialize();
+
+		$campos.prop("disabled", true);
+
+		peticion = $.ajax({
+			url: "https://script.google.com/macros/s/AKfycbx26FrrNQKgKlXZbbVxuEEleQWiYyqsOpE5yZgWHAsAj5y5O0E/exec",
+			type: "post",
+			data: datos
+		});
+
+		peticion.done(function(response, textStatus, jqXHR) {
+			console.log(textStatus);
+		});
+
+		peticion.fail(function(jqXHR, textStatus, errorThrown) {
+			console.log(textStatus);
+		});
+
+		peticion.always(function() {
+			$campos.prop("disabled", false);
+		});
+
+		event.preventDefault();
+
+		alert("Artículo añadido.");
+
+		location.reload();
+	});
+});
